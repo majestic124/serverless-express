2e2458307ddcc88b8c4ab754732f20aa009fc75b
majestic 124 <github.majestic124@passinbox.com>
2024-09-29 17:12:53 +0400

    fix: added error logging

--- src/configure.js
+++ src/configure.js
@@ -83,6 +83,7 @@ function configure ({
           log
         })
       } catch (error) {
+        console.error(error)
         respondToEventSourceWithError({
           error,
           resolver,
